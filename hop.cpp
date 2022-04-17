b473f3c6cd5747829aa5ebde427ca52fce4c8c26
Your Name <nika.neirami@gmail.com>
2022-04-18 01:37:55 +0430

    add

---
+++ hop.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+ {
+ 	int n,m,x;
+ 	cout<<"please enter m";
+ 	cin>>m;
+ 	cout<<"please enter n";
+ 	cin>>n;
+ 	for(int i=1;i<=n;i++)
+ 	{
+ 		 x= i % m;
+ 	if(x==0)
+	 {
+	 	cout<<" "<<"hop";
+	 }
+	 else
+	 {
+	 	cout<<" "<<i;
+	 }
+	 			 
+             
+			  
+	 
+	 	
+	 }
+ 
+	 
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+	return 0;
+}
